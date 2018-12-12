75f00bbdbc0ee8e056f45d46f9f83720110fed87
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-12 18:52:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |word|
+    word.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |word|
+    word.class != String
+  end
+end
+
+def count_elements(array)
+  counts = Hash.new 0
+  array.group_by(&itself).map do |a, b|
+    a.merge(count: b.length)
+  end
+end
+
+def merge_data(keys, data)
+end
